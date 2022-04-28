769981dc31ff0ced2ec99f4b997aa59bd0c68de0
Josué Reyes <josue.hreyess@gmail.com>
2022-04-27 19:04:13 -0500

    Pooling database connections

--- database/index.js
+++ database/index.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 const { promisify } = require('util')
 require('dotenv').config
 
-const conn = mysql.createConnection({
+const pool = mysql.createPool({
   host     : process.env.HOST,
   user     : process.env.USER,
   password : process.env.PASSWORD,
@@ -10,13 +10,22 @@ const conn = mysql.createConnection({
   port     : process.env.DB_PORT
 })
 
-conn.connect((e) => {
-  var status = e ? "Connection Failed" : "Connection initialized... Listening for queries";
-  console.log(`[mysql] ${status}`)
-  if(e) console.log(e)
+pool.getConnection((e, conn) => {
+  if(e){
+    if(e.code === 'PROTOCOL_CONNECTION_LOST'){
+      console.log("[mysql] Connection Lost!")
+    }else if(e.code === 'ER_CON_COUNT_ERROR'){
+      console.log("[mysql] Database has too many connections!")
+    }else if(e. code === 'ECONNREFUSED'){
+      console.log("[mysql] Connection refused!")
+    }
+  }
+
+  if(conn){ conn.release() }
+  console.log("[mysql] Connection initialized! Listening for queries...")
 })
 
 // Promisify queries
-conn.query = promisify(conn.query)
+pool.query = promisify(pool.query)
 
-module.exports = conn
+module.exports = pool
