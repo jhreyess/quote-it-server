9358bbef07b502346fff47d3509b1efd027bf6d5
Josué Reyes <josue.hreyess@gmail.com>
2022-05-18 02:51:11 -0500

    Adds get user likes route

--- routes/api/me.js
+++ routes/api/me.js
@@ -1,13 +1,35 @@
 const router = require('express').Router();
 const auth = require('../middlewares/auth')
+
+const pg = require('pg-promise')();
 const db = require('../../database')
 
+// GET USER FEED
 router.get('/feed', auth, async (req, res) => {
     try{
-        const userPosts = await db.manyOrNone("SELECT * FROM posts WHERE user_id = $1", req.user.id)
+        const exists = pg.as.format("EXISTS(SELECT 1 FROM user_likes WHERE user_likes.post_id = posts.post_id AND user_likes.user_id = $1)", req.user.id)
+        const likes = pg.as.format("SELECT COUNT(*) AS no_likes FROM user_likes WHERE user_likes.post_id = posts.post_id")
+        const userPosts = await db.manyOrNone("SELECT *, ($1:raw) AS liked, ($2:raw) FROM posts WHERE user_id = $3", [exists, likes, req.user.id])
         return res.status(200).json({success: true, data: userPosts})
     }catch(e){
-        return res.status(400).json({success: false})
+        console.log(e)
+        return res.status(400).json({success: false, error: "Something went wrong"})
+    }
+})
+
+// GET USER LIKED POSTS
+router.get('/likes', auth, async (req, res) => {
+    try{
+        const exists = pg.as.format("EXISTS(SELECT 1 FROM user_likes WHERE user_likes.post_id = posts.post_id AND user_likes.user_id = $1)", req.user.id)
+        const likes = pg.as.format("SELECT COUNT(*) AS no_likes FROM user_likes WHERE user_likes.post_id = posts.post_id")
+        const likedPosts = await db.manyOrNone("SELECT *, ($1:raw) FROM posts WHERE ($2:raw)", [likes, exists])
+        likedPosts.forEach(post => {
+            post.liked = true
+        });
+        return res.status(200).json({success: true, data: likedPosts})
+    }catch(e){
+        console.log(e)
+        return res.status(400).json({success: false, error: "Something went wrong"})
     }
 })
 
